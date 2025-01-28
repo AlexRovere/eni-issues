194571fe85416e36db1dc517567140721ba774c0
Antoine COULON <a.antoinecoulon@gmail.com>
2025-01-28 16:07:54 +0100

    V2 ok

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+
+// Mise en place du moteur de rendu EJS
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+// Middleware pour parser le body des requêtes
 app.use(express.urlencoded({ extended: true }));
 
+// Initialisation de la liste des issues
 let issues = [];
 
+// Routes
+// Route pour la page d'accueil
 app.get("/", (req, res) => {
   res.render("index", { issues });
 });
 
+// Route pour la page d'ajout d'une issue
 app.post("/add-issue", (req, res) => {
     let id = issues.length + 1;
     const { titre, description, auteur, date, etat } = req.body;
@@ -20,17 +27,20 @@ app.post("/add-issue", (req, res) => {
     res.redirect("/");
 });
 
+// Route pour la suppression d'une issue
 app.get("/delete-issue/:id", (req, res) => {
     const { id } = req.params;
     issues.splice(id - 1, 1);
     res.redirect("/");
 });
 
+// Route pour la page d'édition d'une issue
 app.get("/edit-issue/:id", (req, res) => {
     const { id } = req.params;
     res.render("edit", { issue: issues[id - 1] });
 });
 
+// Route pour l'édition d'une issue
 app.post("/edit-issue/:id", (req, res) => {
     const { id } = req.params;
     const { titre, description, auteur, date, etat } = req.body;
@@ -38,10 +48,12 @@ app.post("/edit-issue/:id", (req, res) => {
     res.redirect("/");
 });
 
+// Simulation d'une erreur
 app.get('/error', (req, res, next) => {
     next(new Error("Ceci est un test d'erreur"));
 });
 
+// Middleware pour gérer les erreurs
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.render("error", { error: err.message });
