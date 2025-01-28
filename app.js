d982b79d3b4d045fa5d0985ffdb0251f719f9090
Antoine COULON <a.antoinecoulon@gmail.com>
2025-01-28 12:15:07 +0100

    Ajouter: form et affichage issues

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+let issues = [];
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { issues });
+});
+
+app.post("/add-issue", (req, res) => {
+    const { titre, description, auteur, date, etat } = req.body;
+    issues.push({ titre, description, auteur, date, etat });
+    res.redirect("/");
 });
 
 app.listen(port, () => {
