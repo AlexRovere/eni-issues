1c7d64120b1a2f533cce9e13c94cb9559b894e96
Yonni GUERRAULT <yonni4@gmail.com>
2025-01-28 11:32:49 +0100

    arbo

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require("express");
+
+
 const app = express();
 const port = 3000;
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-// premier middleware general
-app.use((req, res, next) => {
-  console.log(
-    `methode=${req.method} url=${req.url} user-agent=${req.headers["user-agent"]}`
-  );
-  
-  next();
-});
+let issues = [];
 
 app.get("/", (req, res) => {
- res.redirect("/index.html");
+  
+  res.render("index", { issues });
 });
 
-
+app.post("/issues/create", (req, res) => {
+  const { auteur, date, titre, description, etat } = req.body;
+  
+  issues.push({ auteur, date, titre, description, etat });
+  res.redirect("/");
+});
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log("Le serveur tourne sur le port " + port);
+
 });
